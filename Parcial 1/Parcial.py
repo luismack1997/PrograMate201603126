c71553d763ccaa125a974f47bbe8ed1d5a609c42
luismack1997 <30278823+luismack1997@users.noreply.github.com>
2017-08-18 12:15:07 -0600

    Agragando Ejercicio del Parcial 1

---
+++ Parcial 1/Parcial.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+suma=0
+
+for x in range(1,8,2):
+    suma+=x*6000+x*600+x*60+x
+
+for x in range(2,9,2):
+    suma+=x*6000+x*600+x*60+x
+
+for x in range(3,10,2):
+    suma+=x*6000+x*600+x*60+x
+
+print suma

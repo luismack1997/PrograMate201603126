6cb827f9c5f3415a4404da1ac93300ac8ef6736a
luismack1997 <30278823+luismack1997@users.noreply.github.com>
2018-05-17 21:35:29 -0600

    Agregando primera parte corregida

--- Proyecto 1 2018/Proyecto1/Automata/views.py
+++ Proyecto 1 2018/Proyecto1/Automata/views.py
@@ -13,12 +13,7 @@ Parentesis={'(',')'}
 Problematicos={'+', '?', '*', '|'}
 Unarios={'+', '?', '*'}
 temporal=''
-temporal2=0
-ora=0
 newtexto=''
-contador_parentesis_iz=0
-contador_parentesis_der=0
-agregador=0
 error=0
 
 def normales(caracter):
@@ -45,16 +40,22 @@ def normales(caracter):
 
 def convertidor(texto):
     global temporal
-    global agregador
     global newtexto
-    global contador_parentesis_iz
-    global temporal2
-    global ora
+    temporal2=0
+    temporal=""
+    ora=0
+    newtexto=''
+    contador_parentesis_iz=0
+    agregador=0
     for caracter in texto:
         if temporal2>0:
             if agregador>0:
                 if newtexto[-1]=='.': 
                     newtexto=newtexto[:-1]
+                    newtexto=newtexto+'|'
+                    agregador-=1
+                    temporal2-=1
+                else: 
                     newtexto=newtexto+'|'
                     agregador-=1
                     temporal2-=1
@@ -86,6 +87,17 @@ def convertidor(texto):
         error=1
     elif contador_parentesis_iz!=0: 
         error=1
+    if temporal2>0:
+            if agregador>0:
+                if newtexto[-1]=='.': 
+                    newtexto=newtexto[:-1]
+                    newtexto=newtexto+'|'
+                    agregador-=1
+                    temporal2-=1
+                else: 
+                    newtexto=newtexto+'|'
+                    agregador-=1
+                    temporal2-=1
     return newtexto
     
 
@@ -94,9 +106,9 @@ def convertidor(texto):
 
 class IndexView(TemplateView):
     template_name='Automata/index.html'
-    def my_view(self, request):
+    def post(self, request, *args, **kwargs):
         if request.method == "POST":
-            screenname = request.POST.get("handle", None)  
+            screenname = request.POST.get("handle")  
             result=convertidor(screenname)
         return render(request, 'Automata/index.html', {'result': result})
         

8bc35425d0a6c374261f1d77b8cd0aa3a9a179f0
luismack1997 <30278823+luismack1997@users.noreply.github.com>
2018-05-31 15:58:01 -0600

    Actualizando babosadas

--- Proyecto 1 2018/Proyecto1/Automata/views.py
+++ Proyecto 1 2018/Proyecto1/Automata/views.py
@@ -16,7 +16,14 @@ Unarios={'+', '?', '*'}
 temporal=''
 newtexto=''
 error=0
-
+numeroEstados=0
+symbolos=[]
+estados_aceptacion=[]
+noEstadosAc=0
+fThompson=[]
+Estados=[]
+estadoInicial=0
+funcionfinal=[] 
 def normales(caracter):
     global temporal
     global newtexto
@@ -259,6 +266,139 @@ def ThompsonExp(texto):
                     ftThom[estadoinicial3].update({'ep1':estadofinal3})
                 except:
                     ftThom[estadoinicial3]={'ep1':estadofinal3}
+    global numeroEstados
+    global symbolos
+    global estados_aceptacion
+    global fThompson
+    global Estados
+    global noEstadosAc
+    global estadoInicial
+    estadoInicial=noEstadosNeg
+    numeroEstados=0
+    symbolos=[]
+    estados_aceptacion=[]
+    noEstadosAc=0
+    fThompson=[]
+    Estados=[]
+    numero=noEstadosNeg
+    while numero<=noEstados:
+        if ftThom[numero]=={}:
+            del ftThom[numero]
+        else:
+            for key in ftThom[numero].keys():
+                if key=='ep' or key=='ep1':
+                    transicion=(numero,'ep',ftThom[numero][key])
+                    fThompson.append(transicion)
+                    if 'ep' in symbolos:
+                        pass 
+                    else: 
+                        symbolos.append('ep')
+                else: 
+                    transicion=(numero,key,ftThom[numero][key])
+                    fThompson.append(transicion)
+                    if key in symbolos:
+                        pass 
+                    else: 
+                        symbolos.append(key)
+            numeroEstados+=1
+            Estados.append(numero)
+        numero+=1
+    numeroEstados+=1
+    estados_aceptacion.append(noEstados-1)
+    noEstadosAc=1
+    Estados.append(noEstados-1)
+
+
+class NFA:
+    def __init__(self,numeroEstados,Estados,symbolos,noEstadosAc,estados_aceptacion,estadoInicial,fThompson):
+        self.num_states = numeroEstados
+        self.states = Estados
+        self.symbols = symbolos
+        self.num_accepting_states = noEstadosAc
+        self.accepting_states = estados_aceptacion
+        self.start_state = estadoInicial
+        self.transition_functions = fThompson
+
+
+class DFA:
+    def __init__(self):
+        self.num_states = 0
+        self.symbols = []
+        self.num_accepting_states = 0
+        self.accepting_states = []
+        self.start_state = 0
+        self.transition_functions = []
+        self.q = []
+    def convert_from_nfa(self, nfa):
+        self.symbols = nfa.symbols
+        self.start_state = nfa.start_state
+
+        nfa_transition_dict = {}
+        dfa_transition_dict = {}
+        
+        # Combine NFA transitions
+        for transition in nfa.transition_functions:
+            starting_state = transition[0]
+            transition_symbol = transition[1]
+            ending_state = transition[2]
+            
+            if (starting_state, transition_symbol) in nfa_transition_dict:
+                nfa_transition_dict[(starting_state, transition_symbol)].append(ending_state)
+            else:
+                nfa_transition_dict[(starting_state, transition_symbol)] = [ending_state]
+
+        self.q.append((0,))
+        
+        # Convert NFA transitions to DFA transitions
+        for dfa_state in self.q:
+            for symbol in nfa.symbols:
+                if len(dfa_state) == 1 and (dfa_state[0], symbol) in nfa_transition_dict:
+                    dfa_transition_dict[(dfa_state, symbol)] = nfa_transition_dict[(dfa_state[0], symbol)]
+                    
+                    if tuple(dfa_transition_dict[(dfa_state, symbol)]) not in self.q:
+                        self.q.append(tuple(dfa_transition_dict[(dfa_state, symbol)]))
+                else:
+                    destinations = []
+                    final_destination = []
+                    
+                    for nfa_state in dfa_state:
+                        if (nfa_state, symbol) in nfa_transition_dict and nfa_transition_dict[(nfa_state, symbol)] not in destinations:
+                            destinations.append(nfa_transition_dict[(nfa_state, symbol)])
+                    
+                    if not destinations:
+                        final_destination.append(None)
+                    else:  
+                        for destination in destinations:
+                            for value in destination:
+                                if value not in final_destination:
+                                    final_destination.append(value)
+                        
+                    dfa_transition_dict[(dfa_state, symbol)] = final_destination
+                        
+                    if tuple(final_destination) not in self.q:
+                        self.q.append(tuple(final_destination))
+
+        # Convert NFA states to DFA states            
+        for key in dfa_transition_dict:
+            self.transition_functions.append((self.q.index(tuple(key[0])), key[1], self.q.index(tuple(dfa_transition_dict[key]))))
+        
+        for q_state in self.q:
+            for nfa_accepting_state in nfa.accepting_states:
+                if nfa_accepting_state in q_state:
+                    self.accepting_states.append(self.q.index(q_state))
+                    self.num_accepting_states += 1
+    def print_dfa(self):
+        global funcionfinal
+        print(len(self.q))
+        print("".join(self.symbols))
+        print(str(self.num_accepting_states) + " " + " ".join(str(accepting_state) for accepting_state in self.accepting_states))
+        print(self.start_state)
+        funcionfinal=self.transition_functions
+        
+        for transition in sorted(self.transition_functions):
+            print(" ".join(str(value) for value in transition))
+
+def dibujador(): 
     global ubicacionlatex
     ubicacionlatex=os.path.dirname( os.path.realpath(__file__) )+"\\"+ubicacionlatex
     fichero = open(ubicacionlatex, 'w')
@@ -292,18 +432,13 @@ def ThompsonExp(texto):
     fichero.write('\n')
     fichero.write("\\begin{tikzpicture}[shorten >=1pt,node distance=2cm,on grid,auto] ")
     fichero.write('\n')
-    s=0
     numero=noEstadosNeg
-    salidos=[]
-    guardador=[]
     while numero<=noEstados:
-        d=0
         if numero in ftThom:  
             if s==0:
                 fichero.write("\\node[state,initial] (q_0) {$q_0$};")
                 fichero.write('\n')
-                salidos.append(numero)
-                guardador.append(s)
+
                 s+=1
             else:
                 for x in salidos:
@@ -364,9 +499,13 @@ def ThompsonExp(texto):
     fichero.write('\\end{document}')
     fichero.close()
     cadenaCompilacion = "cd " +  os.path.dirname( os.path.realpath(__file__) )+ " & pdflatex automata.tex"
-    subprocess.Popen(cadenaCompilacion, shell=True, stdout =subprocess.PIPE).stdout.read()
-    print ftThom
-ThompsonExp('ab|')  
+    subprocess.Popen(cadenaCompilacion, shell=True, stdout =subprocess.PIPE).stdout.read() 
+
+ThompsonExp('ab|c.')
+Automata=NFA(numeroEstados,Estados,symbolos,noEstadosAc,estados_aceptacion,estadoInicial,fThompson)
+AutomataFinal=DFA()
+AutomataFinal.convert_from_nfa(Automata)
+AutomataFinal.print_dfa()
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -376,7 +515,16 @@ class IndexView(TemplateView):
             screenname = request.POST.get("handle")  
             result=convertidor(screenname)
             ThompsonExp(result)
-
+            global numeroEstados
+            global symbolos
+            global estados_aceptacion
+            global fThompson
+            global Estados
+            global noEstadosAc
+            global estadoInicial
+            Automata=NFA(numeroEstados,Estados,symbolos,noEstadosAc,estados_aceptacion,estadoInicial,fThompson)
+            AutomataFinal=DFA()
+            AutomataFinal.convert_from_nfa(Automata)
         return render(request, 'Automata/index.html', {'result': result})
 
 
